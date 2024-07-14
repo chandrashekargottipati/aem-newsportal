6a7690ef510a1849b273564e67e76da3ec98fd0c
chandra shekar <chandrashekargottipati@gamil.com>
2024-07-15 00:05:50 +0530

    add For forkify config restapi

---
+++ core/src/main/java/com/gofortrainings/newsportal/core/config/ForkifyApiConfig.java
@@ -0,0 +1,14 @@
+package com.gofortrainings.newsportal.core.config;
+
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+
+@ObjectClassDefinition(name = "Forkify API Configuration")
+public @interface ForkifyApiConfig {
+
+    @AttributeDefinition(name = "API Base URL", description = "Base URL for the Forkify API")
+    String apiBaseUrl() default "https://forkify-api.herokuapp.com/api/v2/recipes";
+
+    @AttributeDefinition(name = "Enable API", description = "Enable or disable the Forkify API integration")
+    boolean enableApi() default true;
+}
---
+++ core/src/main/java/com/gofortrainings/newsportal/core/services/ForkifyApiService.java
@@ -0,0 +1,32 @@
+package com.gofortrainings.newsportal.core.services;
+
+import com.gofortrainings.newsportal.core.config.ForkifyApiConfig;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.metatype.annotations.Designate;
+
+@Component(service = ForkifyApiService.class)
+@Designate(ocd = ForkifyApiConfig.class)
+public class ForkifyApiService {
+
+    private ForkifyApiConfig config;
+
+    @Activate
+    @Modified
+    protected void activate(ForkifyApiConfig config) {
+        this.config = config;
+    }
+
+    public String getFullApiUrl(String recipeId) {
+        if (config.enableApi()) {
+            return config.apiBaseUrl() + "/" + recipeId;
+        }
+        return null; // or handle disabled state as appropriate
+    }
+
+    public boolean isApiEnabled() {
+        return config.enableApi();
+    }
+}
